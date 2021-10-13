49b80aaec9bb5b41684d9c78718ecc97bddc03bb
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-13 18:03:36 -0400

    Excerise 41 (NO TESTING/NO DIAGRAM)

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -4,40 +4,72 @@
  */
 
 package baseline;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution41 {
 
+    // LoadStringArray();
+    public static void LoadStringArray(Scanner input, ArrayList<String> namesList) {
 
-    //LoadStringArray()
+        while (input.hasNextLine()) {
+            // - load contents of file in an array
+            namesList.add(input.nextLine());
+        }
+
+    }
+
+    public static void AlphabetizeArray(ArrayList<String> namesList) {
+        // - take the Array given and alphabetize it;
+        Collections.sort(namesList);
+    }
+
+    public static void displayArray(ArrayList<String> namesList) throws IOException
+    {
+        FileWriter output = new FileWriter("Data/exercise41_output.txt");
+
+        output.write("Total of " + namesList.size() + " names\n");
+
+        output.write("--------------------\n");
+
+        for(String i: namesList)
+        {
+            output.write(i+"\n");
+        }
+
+        output.close();
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
 
         // - load in file
-
-        // - load contents of file in an array
-
-
-    //AlphabetizeArray()
-
-        // - take the Array given and alphabetize it;
-
-    //DisplayArray()
-
-         // - take the current array and Display it
-
-    //public static void main(String[] args) {
+        Scanner inputFile = new Scanner(new File("Data/exercise41_input.txt"));
+        ArrayList<String> namesList = new ArrayList<String>();
 
         // LoadStringArray();
+        LoadStringArray(inputFile, namesList);
 
         // AlphabetizeArray();
+        AlphabetizeArray(namesList);
 
         // DisplayArray();
+        try
+        {
+            displayArray(namesList);
+        }
 
-
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 
 
     //  **** USE J-UNIT 5 TO UNIT TEST *****
     //  **** INCORPORATE PLANT UML CLASS DIAGRAMS ******
-    
+
 
 }
 
