36b0dc42bcaed08832dcebebd12da42126ac7046
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-13 18:05:12 -0400

    exercise42 Outline

---
+++ exercise42/src/main/java/baseline/DisplayInfo.java
@@ -0,0 +1,14 @@
+package baseline;
+
+public class DisplayInfo {
+    public  static void displayFileInfo(){
+
+        //take string 1
+        //take string 2
+        //take string 3
+
+        //printf( " Last ",  " First ", "Salary")
+        // printf(" StringArray[1], stringArray2, stringArray3")
+
+    }
+}
---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,32 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+
+package baseline;
+
+public class Solution42 {
+
+        //LOAD FILE FUNCTION(){
+        //    - TRY
+        //          -to establish the file
+        //    - establish the scanner
+
+        //    - WHILE (FILE.NEXT.LN)
+        //          - scan in the contents of that line
+        //    - CATCH EXCEPTION
+        //          - ""File not Found""
+
+    public static void main(String[] args) {
+
+        // LOAD FILE FUNCTION()
+
+        // DisplayInfoClass (StringArray1, StringArray2, StringArray3)
+
+    }
+}
+
+
+
+
