2c18c9a9945b1fcdafe456cc81c9d396dbd1ac60
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-14 08:48:22 -0400

    Exercise 43 Outline

---
+++ exercise43/src/main/java/baseline/createHTML.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class createHTML {
+
+    //Takes the name of the site
+    //Takes the author of the site
+
+    //asks do you want a folder for Javascript
+    //asks if they want a folder for css
+
+    // ***?***Generate index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.**?**
+}
---
+++ exercise43/src/main/java/baseline/exercise43.java
@@ -0,0 +1,23 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+
+public class exercise43 {
+
+        //LoadInformation (String siteName, String AuthorName)
+    //     {
+        //             Prompt for site name
+        //             Prompt for author name
+        //  }
+
+    
+    public static void main(String[] args) {
+
+        // LoadInformation (String siteName, String AuthorName)
+
+        // call createHTML Class
+    }
+}
