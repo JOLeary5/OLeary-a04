8f0e9a6a38c4266236a3df9513ffc11f58787ccf
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-17 11:14:30 -0400

    Exercise 45 OutLine

---
+++ exercise45/src/main/java/baseline/FindAndReplace.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class FindAndReplace
+{
+    // FUNCTION replacmentWord(){
+    // MAP = NEWHASHMAP
+    // replace word
+    // return map
+
+    // FUNCTION replaceWords(){
+    //
+    //   Somehow replace words using a hashmap
+    //
+    //
+    public static void FindAndReplaceWithHashmaps()
+    {
+        // NEW MAP = replaceWord();
+
+        //try
+        //   - streaming the output file we created
+        //   - replace words in the output file line by line
+
+    }
+}
---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,23 @@
+package baseline;
+
+public class Solution45 {
+
+        // FUNCITON InputFileCopy{
+        //    - initialize filereader
+        //    - Initialize file Scanner
+        //          - while file scanner !=NULL
+        //              - Copy to outfile
+        //    - close files
+        //  }
+
+    public static void main(String[] args) {
+        // initialize Scanner
+
+        // Prompt for name of the output file
+
+        // FUNCTION InputFileCopy
+
+        // CLASS FindAndReplace
+        
+    }
+}
