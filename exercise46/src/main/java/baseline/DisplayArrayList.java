6e72594a6d0dc3ad23b1a6015f007337e1019d52
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-17 10:19:02 -0400

    Exercise 46 Outline

---
+++ exercise46/src/main/java/baseline/DisplayArrayList.java
@@ -0,0 +1,8 @@
+package baseline;
+
+public class DisplayArrayList {
+    // for the size of the arraylist
+    //     - print out word
+    //    - for # of times it's been counted
+    //    -  print ***
+}
---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,28 @@
+package baseline;
+
+public class Solution46 {
+
+    // FUNCTION CREATE ARRAYLISTS{
+
+    // - initialize scanner object
+
+    //  - initialize arraylist for words
+    //  - initialize arraylist for Count
+
+    // - While (HasNext)
+    //       - String nextword = scanner.next
+
+    //       -  if contains word
+    //              - wordcount ++
+
+    //       - else
+    //              - add word
+    //  - close files
+
+    public static void main(String[] args) {
+
+        // FUNCTION CREATE ARRAYLIST
+        // CLASS DISPLAY ARRAY
+
+    }
+}
