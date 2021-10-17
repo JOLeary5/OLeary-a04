fd1fb97584791be010ea65810642cbf855ae9475
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-17 11:46:49 -0400

    Exercise45 (Missing JNUIT Testing)

--- exercise45/src/main/java/baseline/FindAndReplace.java
+++ exercise45/src/main/java/baseline/FindAndReplace.java
@@ -1,24 +1,57 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
 package baseline;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FindAndReplace
 {
-    // FUNCTION replacmentWord(){
-    // MAP = NEWHASHMAP
-    // replace word
-    // return map
-
-    // FUNCTION replaceWords(){
-    //
-    //   Somehow replace words using a hashmap
-    //
-    //
-    public static void FindAndReplaceWithHashmaps()
+    public static Map<String,String> replaceWith()
     {
-        // NEW MAP = replaceWord();
+        //Creating New-Hashmap
+        Map<String,String>map = new HashMap<>();
+        map.put("utilize","use");
+        return map;
+    }
 
-        //try
-        //   - streaming the output file we created
-        //   - replace words in the output file line by line
+    public static String replaceWords(String str, Map<String,String> map)
+    {
+        for (Map.Entry<String,String> entry : map.entrySet())
+        {
+            //Replace the value with the Hashmap Key
+            if (str.contains(entry.getKey())){
+                str = str.replace(entry.getKey(), entry.getValue());
+            }
+        }
+        return str;
+    }
+
+    public static void FindAndReplaceWithHashmaps(String name)
+    {
+        Map<String, String> hashMap = replaceWith();
+
+        //Trying to replace the Strings in the File according to the HASMAP
+        try
+        {
+            Stream<String> lines = Files.lines(Path.of("Data/" + name), Charset.forName("UTF-8"));
+            List<String> replacedLine = lines.map(line-> replaceWords(line,hashMap)).collect(Collectors.toList());
+            Files.write(Path.of("Data/"+name),replacedLine, Charset.forName("UTF-8"));
+            lines.close();
+            System.out.println("Done");
+        }
+        catch (IOException e )
+        {
+            e.printStackTrace();
+        }
 
     }
 }
--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -1,23 +1,44 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
 package baseline;
+
+import java.io.*;
+import java.util.*;
+
 
 public class Solution45 {
 
-        // FUNCITON InputFileCopy{
-        //    - initialize filereader
-        //    - Initialize file Scanner
-        //          - while file scanner !=NULL
-        //              - Copy to outfile
-        //    - close files
-        //  }
+    public static void InputFileCopy(String fileName) throws IOException {
+        FileReader fileIN = new FileReader("Data/exercise45_input.txt");
+        FileWriter fileOUT = new FileWriter("Data/"+fileName, true);
 
-    public static void main(String[] args) {
-        // initialize Scanner
+        int c;
+        while ((c = fileIN.read()) != -1) {
+            fileOUT.write(c);
+        }
+        System.out.println("Copy finish...");
+        fileIN.close();
+        fileOUT.close();
 
-        // Prompt for name of the output file
+    }
 
-        // FUNCTION InputFileCopy
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        //prompt for name of the file
+        System.out.print("Please name the output file:");
+        String fileName = sc.next();
+
+        //FUNCTION InputFileCopy
+        InputFileCopy(fileName);
 
         // CLASS FindAndReplace
-        
+        FindAndReplace.FindAndReplaceWithHashmaps(fileName);
+
+
+
     }
 }
