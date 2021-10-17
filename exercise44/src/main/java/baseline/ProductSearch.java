e61ea365ecf52971c706f0fc2c70cb58601e0017
JOLeary5 <jonathanoleary2@knights.ucf.edu>
2021-10-17 16:27:30 -0400

    exercise44 Outline

---
+++ exercise44/src/main/java/baseline/ProductSearch.java
@@ -0,0 +1,34 @@
+package baseline;
+
+public class ProductSearch {
+    public static void JSONProductSearch(){
+
+        // Establish the String Input
+        // Establish Flag int to test if given string is present
+
+
+        // Use a buffered reader to take input
+
+        // Prompt and take user File Name
+
+        // !!!!!CREATE AN ITERATOR TO LOOP THROUGH PRODUCTS ARRAY!!!!!!!!!!!
+
+        // While (Iterator.hasNext()) {                             (GOOGLED THIS SOLUTION)
+        //
+        //       - Use ITERATOR TO SEARCH THE JSON PRODUCT LIST
+        //       - STRING name = json.get(NAME)
+        //       - While( itereator has next)
+        //                  - compare value with given input
+        //                       - if (input to lowerCase equals (snametolower)
+        //                             - sout NAME
+        //                             - sout price
+        //                             - get quantity
+        //                             - flag = 1;
+        //       if (flag ===0){
+        //              sout("PRODCUT NOT FOUND")
+        //        }
+        //
+
+
+    }
+}
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,30 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 4 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+public class Solution44
+{
+    // FUNCTION READ JSON FILE
+    // {
+    //        -  Create new JSON Praser object
+    //
+    //        -  Store JSON Products into an array
+    //
+    //}
+
+    public static void main(String[] args)
+    {
+
+        //FUNCTION READ-JSON FILE
+
+        //CLASS Product Search
+    }
+}
